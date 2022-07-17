f820c48afa78ad0a1fcc0dd3ad423a1313c73176
DreamOfIce <dreamofice@outlook.com>
2022-07-18 00:18:28 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cloudflareScraper = require('cloudflare-scraper');
+const app = express();
+
+app.get('/api/*', (req, res) => {
+    const link = req.url.slice(5);
+    console.log(link);
+    cloudflareScraper.get(link, { encoding: null }).then((body) => {
+        res.status(200).send(body).end();
+    }).catch((err) => {
+        res.status(500).json({ 'status': err, 'message': err });
+    })
+})
+
+app.use('/', express.static(path.join(__dirname, public)))
+app.listen(process.env.PORT || 8080);
